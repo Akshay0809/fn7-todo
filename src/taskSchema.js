92cad8a7f850ac96ad2cef275552705ce1e45d2b
Akshay0809 <akshaythatikonda>
2023-11-04 15:19:27 +0530

    Revert "first commit"
    This reverts commit 50ba8a2bd6ad9fcba96195eee41a004ec5283e60.

--- src/taskSchema.js
+++
@@ -1,9 +0,0 @@
-export const taskSchema = {
-    type: "object",
-    properties: {
-      title: { type: "string" },
-      description: { type: "string" },
-      dueDate: { type: "string", format: "date" },
-      priority: { type: "string", enum: ["Low", "Medium", "High"] },
-    },
-  };
--- src/taskUISchema.js
+++
@@ -1,9 +0,0 @@
-export const taskUISchema = {
-    type: "VerticalLayout",
-    elements: [
-      { type: "Control", label: "Title", scope: "#/properties/title" },
-      { type: "Control", label: "Description", scope: "#/properties/description" },
-      { type: "Control", label: "Due Date", scope: "#/properties/dueDate" },
-      { type: "Control", label: "Priority", scope: "#/properties/priority" },
-    ],
-  };
